1cb61875f9a53548261a747bf39fbb3c556cecd3
michaelrusek <de.mrusso@gmail.com>
2023-07-08 10:37:29 +0200

    fix: remove useState import

--- client/src/components/Transactions/Transactions.tsx
+++ client/src/components/Transactions/Transactions.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import styles from "./Transactions.module.scss";
 import TransactionType from "../../types/TransactionType";
 import Transaction from "../Transaction/Transaction";
